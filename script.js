f79dc5eded84861ff0e2cf5f41bc7c5b9efa28f0
MDTufan <129486543+MDTufan@users.noreply.github.com>
2023-06-15 04:29:49 -0700

    Add files via upload
    fast comite

---
+++ script.js
@@ -0,0 +1,77 @@
+
+// var students = ["Tufan ali", "Toli", "Priya", "rubel"];
+// console.log("students = " + students);
+
+// console.log( students[2]);
+
+
+// console.log(students.length);
+
+
+// students.push("musa");
+// console.log("after pushing students = " + students);
+
+
+// students.pop("musa");
+// console.log("after poping students = " + students);
+ 
+
+// students.unshift("Roki");
+// console.log("after poping students = " + students);
+
+// students.shift();
+// console.log("after poping students = " + students);
+
+
+
+
+
+// var num1 = [10, 20];
+// var num2 = [30, 40, 50, 60];
+// var num = num1.concat(num2);
+// console.log(num);
+
+
+
+
+
+
+
+
+// var num1 = 20;
+// var num2 = 10;
+// var num3 = "10";
+// var num4 = 20;
+// var num5 = 15;
+
+// console.log(num1 > num2); 
+// console.log(num1 >= num2);
+// console.log(num1 < num2);
+// console.log(num1 <= num2);
+// console.log(num1 == num4);
+// console.log(num1 != num4);
+// console.log(num1 === num3);
+// console.log("num2 === num3 : " + (num2 === num3));
+// console.log("num2 == num3 : " + (num2 == num3));
+// console.log(num1 !== num3);
+
+// logical operators helps us to compine multiple conditions
+// console.log(num1 > num2 && num1 > num5);
+// console.log(num1 > num2 || num1 > num5);
+
+
+
+
+
+
+// 2222222222222
+// var num1= 12;
+// var num2= 8;
+
+// if (num1 > num2) {
+//     console.log("Large Number is : " + num1);
+//   } else if (num1 < num2) {
+//     console.log("Large Number is : " + num2);
+//   } else if (num1 == num2) {
+//     console.log("Equal numbers");
+//   }
